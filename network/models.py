42497924d199485ca8dad31f7c723aa53dd071db
Juan Cruz Donato <juandonato@gmail.com>
2020-11-04 15:09:55 -0300

    Post Model

--- network/models.py
+++ network/models.py
@@ -4,3 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    message = models.CharField(max_length=280)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="usrPosts", default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def post(self, message, user):
+        self.message = message
+
+    def __str__(self):
+        return self.message
--- network/tests.py
+++ network/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from .models import Post, User
+from datetime import datetime
+from django.utils import timezone
+
+def createUser(username, email, password):
+    u = User()
+    u.username = username
+    u.email = email
+    u.password = password
+    u.save()
+    return u
 
 # Create your tests here.
+class TestsPost(TestCase):
+
+    def test_get_post_message(self):
+        """*** Printing the post needs to be equal to the post message ***"""
+        u = createUser("foo", "foo@example.com", "exmaple")
+        m = "New post message"
+        p = Post()
+        p.post(m, u)
+        self.assertEqual(str(p), m)
+
+    def test_get_post_user(self):
+        """*** Post needs to be posted by a User ***"""
+        u = createUser("foo", "foo@example.com", "exmaple")
+        m = "New post message"
+        p = Post()
+        p.post(m, u)
+        self.assertEqual(p.user, u)
+
+    def test_get_post_timestamp(self):
+        """*** Post needs to be timestamped ***"""
+        u = createUser("foo", "foo@example.com", "exmaple")
+        m = "New post message"
+        p = Post()
+        p.post(m, u)
+        p.save()
+        self.assertTrue(abs(p.created_at - timezone.now()) < timezone.timedelta(seconds=5))
+
+if __name__ == "__main__":
+    unittest.main()
