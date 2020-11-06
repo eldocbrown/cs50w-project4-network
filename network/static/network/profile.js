c759c25f70bb1ec72d3e88ea3e6d82a4154d9603
Juan Cruz Donato <juandonato@gmail.com>
2020-11-05 23:12:21 -0300

    Follow button behaviour based con data-following value

--- network/static/network/profile.js
+++ network/static/network/profile.js
@@ -1,14 +1,48 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-  // if buttonFollow exists
-  const buttonFollow = document.querySelector('#buttonFollow')
+  evaluateFollowStatus();
+  const buttonFollow = document.querySelector('#buttonFollow');
   if (buttonFollow !== null) {
-    // fetch users that I follow
-    // if i am following this users
-      // show button text FOLLOWING (filled blue)
-      // show button text UNFOLLOW on hover (filled red)
-    // else
-      // show button text FOLLOW (outlined blue)
+    buttonFollow.addEventListener('click', (event) => {
+      handleFollowClick(event);
+    });
   }
-
 })
+
+function evaluateFollowStatus() {
+  // if buttonFollow exists
+  const buttonFollow = document.querySelector('#buttonFollow');
+  if (buttonFollow !== null) {
+    // if i am following this users
+    if (buttonFollow.dataset.following === 'True') {
+      // show button text FOLLOWING (filled blue)
+      buttonFollow.className = 'btn-sm btn-primary rounded-pill';
+      buttonFollow.innerHTML = 'Following';
+      // show button text UNFOLLOW on mouseover (filled red)
+      buttonFollow.addEventListener('mouseover', (event) => {
+        buttonFollow.className = 'btn-sm btn-danger rounded-pill';
+        buttonFollow.innerHTML = 'Unfollow';
+      });
+      // show button text UNFOLLOW on mouseover (filled red)
+      buttonFollow.addEventListener('mouseout', (event) => {
+        buttonFollow.className = 'btn-sm btn-primary rounded-pill';
+        buttonFollow.innerHTML = 'Following';
+      });
+    } else {
+      // show button text FOLLOW (outlined blue)
+      buttonFollow.className = 'btn-sm btn-outline-primary rounded-pill';
+      buttonFollow.innerHTML = 'Follow';
+    }
+  }
+}
+
+function handleFollowClick(event) {
+  if (event.currentTarget.dataset.following === 'True') {
+    // TODO: put unfollow
+    // TODO: .then event.currentTarget.dataset.following = False
+  } else {
+    // TODO: put follow
+    // TODO: .then event.currentTarget.dataset.following = True
+  }
+  evaluateFollowStatus();
+  event.currentTarget.blur();
+}
